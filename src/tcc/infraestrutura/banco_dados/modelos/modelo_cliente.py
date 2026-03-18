8fb1100d674c787d0f1ec353d38a49649f246d2f
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-03-17 21:34:21 -0300

    Criado pasta de modelos, criado modelo base, criado modelo cliente.

---
+++ src/tcc/infraestrutura/banco_dados/modelos/modelo_cliente.py
@@ -0,0 +1,43 @@
+from uuid import UUID
+from sqlalchemy import Column, String, Integer
+from tcc.infraestrutura.banco_dados.modelos.modelo_base import ModeloBase
+
+
+class ModeloCliente(ModeloBase):
+    __tablename__ = 'clientes'
+
+    id= Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        nullable=False
+    )
+
+    nome= Column(
+        String(100),
+        nullable=False
+    )
+
+    codigo = Column(
+        Integer(10),
+        nullable=False
+    )
+
+    tipo = Column(
+        String(12),
+        nullable=False
+    )
+
+    celular = Column(
+        Integer(11),
+        nullable=False
+    )
+
+    email = Column(
+        String(40),
+        nullable=False
+    )
+
+    como_encontrou = Column(
+        String(),
+        nullable=True
+    )
