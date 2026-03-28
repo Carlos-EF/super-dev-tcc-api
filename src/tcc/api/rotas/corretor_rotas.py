87c315980837d80f5ceb732c19e947137a09ea9b
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-03-28 09:01:57 -0300

    Criado rota para obter corretor filtrando por seu ID.

--- src/tcc/api/rotas/corretor_rotas.py
+++ src/tcc/api/rotas/corretor_rotas.py
@@ -34,3 +34,33 @@ def listar_corretores(
     repositorio = RepositorioCorretor(sessao=session)
     corretores = repositorio.listar()
     return corretores
+
+
+@router.get(
+    '{id}',
+    response_model= CorretorResponse,
+    status_code=status.HTTP_200_OK,
+    summary='Busca um corretor filtrando por seu ID.',
+    description="""
+        Busca um corretor específico pelo seu ID (UUID v7).""",
+    responses= {
+        200: {
+            'description': 'Corretor encontrado',
+            'model': CorretorResponse
+        },
+    },
+)
+def buscar_corretor(
+    id: UUID,
+    session: Session = Depends(obter_sessao)
+):
+    """Listagem de todos os corretores cadastrados."""
+    repositorio = RepositorioCorretor(sessao=session)
+    corretor = repositorio.obter_por_id(id)
+    if not corretor:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Corretor não encontrado.'
+        )
+    
+    return corretor
