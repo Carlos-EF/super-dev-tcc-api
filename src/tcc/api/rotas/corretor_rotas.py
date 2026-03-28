db2b71ee1dfff5c9b6e0b358e4fb4f49bf45d426
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-03-28 08:56:51 -0300

    Criado arquivo de rotas para corretores; criado função de obter todos os corretores; alterado o nome do arquivo de rotas de clientes.

---
+++ src/tcc/api/rotas/corretor_rotas.py
@@ -0,0 +1,36 @@
+from http import HTTPStatus
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from uuid6 import uuid7
+
+
+from tcc.infraestrutura.conexao import obter_sessao
+from tcc.infraestrutura.banco_dados.modelos.modelo_corretor import ModeloCorretor
+from tcc.infraestrutura.repositorios.repositorio_corretor import RepositorioCorretor
+from tcc.api.schemas.corretor_schemas import CorretorResponse, CriarCorretorRequest, AlterarCorretorRequest
+
+
+router = APIRouter(
+    prefix='/corretores',
+    tags=['Corretores'],
+)
+@router.get(
+    '',
+    response_model=list[CorretorResponse],
+    status_code=status.HTTP_200_OK,
+    summary='Listar todos os corretores cadastrados.',
+    responses= {
+        200: {
+            'description': 'Lista dos corretores cadastrados',
+            'model': list[CorretorResponse]
+        },
+    },
+)
+def listar_corretores(
+    session: Session = Depends(obter_sessao)
+):
+    """Listagem de todos os corretores cadastrados."""
+    repositorio = RepositorioCorretor(sessao=session)
+    corretores = repositorio.listar()
+    return corretores
