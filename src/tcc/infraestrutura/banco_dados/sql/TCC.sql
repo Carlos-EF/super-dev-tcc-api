faef9e67794554f4cbb8075703398b0c754dbb68
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-05-12 10:16:34 -0300

    Adicionado arquivo SQL; criado pasta de SQL para facilitar instalação do projeto em maquinas novas.

---
+++ src/tcc/infraestrutura/banco_dados/sql/TCC.sql
@@ -0,0 +1,102 @@
+CREATE TABLE clientes (
+    id UUID PRIMARY KEY NOT NULL,
+
+    nome VARCHAR(100) NOT NULL,
+
+    codigo INTEGER NOT NULL,
+
+    tipo VARCHAR(12) NOT NULL,
+
+    celular VARCHAR(14) NOT NULL,
+
+    email VARCHAR(40) NOT NULL,
+
+    como_encontrou VARCHAR(14),
+
+    status VARCHAR(7) NOT NULL
+);
+
+
+CREATE TABLE interessados (
+    id UUID PRIMARY KEY NOT NULL,
+
+    id_cliente UUID NOT NULL UNIQUE,
+
+    procurando VARCHAR(11) NOT NULL,
+
+    orcamento INTEGER NOT NULL,
+
+    orcamento_minimo INTEGER,
+
+    orcamento_maximo INTEGER,
+
+    quantidade_quartos INTEGER,
+
+    quantidade_suites INTEGER,
+
+    quantidade_banheiros INTEGER,
+
+    quantidade_vagas_garagem INTEGER,
+
+    quantidade_andares INTEGER,
+
+    quantidade_salas INTEGER,
+
+    CONSTRAINT fk_interessado_cliente
+        FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id)
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE proprietarios (
+    id UUID PRIMARY KEY NOT NULL,
+
+    id_cliente UUID NOT NULL UNIQUE,
+
+    imovel_proprietario TEXT,
+
+    CONSTRAINT fk_proprietario_cliente
+        FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id)
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE locatarios (
+    id UUID PRIMARY KEY NOT NULL,
+
+    id_cliente UUID NOT NULL UNIQUE,
+
+    imovel_locatario TEXT,
+
+    CONSTRAINT fk_locatario_cliente
+        FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id)
+        ON DELETE CASCADE
+);
+
+
+CREATE TABLE corretores (
+    id UUID PRIMARY KEY NOT NULL,
+
+    status VARCHAR(7) NOT NULL,
+
+    tipo VARCHAR(12) NOT NULL,
+
+    nome_completo VARCHAR(60) NOT NULL,
+
+    codigo INTEGER NOT NULL,
+
+    celular VARCHAR(15) NOT NULL,
+
+    email VARCHAR(50) NOT NULL,
+
+    creci VARCHAR(7) NOT NULL,
+
+    data_nascimento VARCHAR(10),
+
+    rg VARCHAR(9),
+
+    cpf VARCHAR(14)
+);
