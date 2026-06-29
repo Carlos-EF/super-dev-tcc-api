0336366ee7ad563bbb475396d92ba9a2afd6b889
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-06-29 09:55:24 -0300

    Criado rota para fazer a listagem de imóveis.

--- src/tcc/api/rotas/imoveis_rotas.py
+++ src/tcc/api/rotas/imoveis_rotas.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
+from src.tcc.api.schemas.imovel_schemas import ImovelResponse
+from src.tcc.infraestrutura.repositorios.repositorio_imovel import RepositorioImovel
 from tcc.infraestrutura.conexao import obter_sessao
 from sqlalchemy.orm import Session
 
@@ -7,3 +9,24 @@ router = APIRouter(
     prefix='/imoveis',
     tags=['Imóveis'],
 )
+@router.get(
+    '',
+    response_model=list[ImovelResponse],
+    status_code=status.HTTP_200_OK,
+    summary='Listar todos os imóveis cadastrados.',
+    responses= {
+        200: {
+            'description': 'Lista dos imóveis cadastrados',
+            'model': list[ImovelResponse]
+        },
+    },
+)
+def listar_imoveis(
+    session: Session = Depends(obter_sessao)
+):
+    """Listagem dos imóveis cadastrados."""
+    repositorio = RepositorioImovel(sessao=session)
+
+    imoveis = repositorio.listar_imoveis()
+
+    return imoveis
