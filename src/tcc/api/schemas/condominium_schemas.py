1c496a1853b0512d07f407a266a37b6090a0bebb
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-24 08:12:07 -0300

    Adicionado schema de response de condomínios.

--- src/tcc/api/schemas/condominium_schemas.py
+++ src/tcc/api/schemas/condominium_schemas.py
@@ -142,5 +142,85 @@ class EditCondominiumRequest(BaseModel):
                     'cidade': 'Blumenau'
                 }
             ]
+        }  
+    }
+
+
+class CondominiumResponse(BaseModel):
+    id: UUID = Field(
+        ...,
+        description='Identificador único (UUID v7) do condomínio cadastrado',
+        examples=['019eaeb2-5ad8-7d22-abca-1b32e99b734a']
+    )
+
+
+    nome: str = Field(
+        ...,
+        min_length=3,
+        max_length=60,
+        description='Nome do condomínio',
+        examples=['Atlanta']
+    )
+
+    cep: str = Field(
+        ...,
+        min_length=9,
+        max_length=9,
+        description='CEP do condomínio',
+        examples=['89040-001']
+    )
+
+    logradouro: str = Field(
+        ...,
+        min_length=3,
+        max_length=60,
+        description='Logradouro do condomínio',
+        examples=['Rua dos Caçadores']
+    )
+
+    numero: int = Field(
+        ...,
+        description='Número do condomínio',
+        examples=[204]
+    )
+
+    bairro: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        description='Bairro do condomínio',
+        examples=['Velha']
+    )
+
+    uf: str = Field(
+        ...,
+        min_length=2,
+        max_length=2,
+        description='UF onde se localiza o condomínio',
+        examples=['SC']
+    )
+
+    cidade: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        description='Cidade do condomínio',
+        examples=['Blumenau']
+    )
+
+    model_config = {
+        'json_schema_extra': {
+            'examples': [
+                {
+                    'id': '019eaeb2-5ad8-7d22-abca-1b32e99b734a',
+                    'nome': 'Atlanta',
+                    'cep': '89040-001',
+                    'logradouro': 'Rua dos Caçadores',
+                    'numero': 204,
+                    'bairro': 'Velha',
+                    'estado': 'SC',
+                    'cidade': 'Blumenau'
+                }
+            ]
         }
     }
