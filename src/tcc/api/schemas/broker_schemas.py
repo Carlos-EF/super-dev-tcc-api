e2149129507dd62f4c2f0b1e4f0151f73707bdfe
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-30 07:57:26 -0300

    Criado schema de editar.

--- src/tcc/api/schemas/broker_schemas.py
+++ src/tcc/api/schemas/broker_schemas.py
@@ -83,3 +83,76 @@ class CreateBrokerRequest(BaseModel):
             ]
         }
     }
+
+
+class EditBrokerRequest(BaseModel):
+    nome: str = Field(
+        ...,
+        min_length=3,
+        max_length=60,
+        description='Nome do corretor',
+        examples=['João Batista']
+    )
+
+    creci: str = Field(
+        ...,
+        min_length=7,
+        max_length=7,
+        description='Creci do corretor',
+        examples=['12.345F']
+    )
+
+    numero: str = Field(
+        ...,
+        min_length=15,
+        max_length=15,
+        description='Número de celular do corretor',
+        examples=['(47) 91234-5678']
+    )
+
+    email: str = Field(
+        ...,
+        max_length=60,
+        description='E-mail do corretor',
+        examples=['joaobatis@corretor.com']
+    )
+
+    data_nascimento: str | None = Field(
+        None,
+        min_length=10,
+        max_length=10,
+        description='Data de nascimento do corretor',
+        examples=['07/01/1992']
+    )
+
+    rg: str | None = Field(
+        None,
+        min_length=9,
+        max_length=9,
+        description='RG do corretor',
+        examples=['1.234.567']
+    )
+
+    cpf: str | None = Field(
+        None,
+        min_length=14,
+        max_length=14,
+        description='CPF do corretor',
+        examples=['123.456.789-00']
+    )
+
+    model_config = {
+        'json_schema_extra': {
+            'examples': [
+                {
+                    'nome': 'João Batista',
+                    'creci': '12.345F',
+                    'celular': '(47) 91234-5678',
+                    'email': 'joaobatis@corretor.com',
+                    'data_nascimento': '07/01/1992',
+                    'rg': '1.234.567',
+                    'cpf': '123.456.789-00'
+                }
+            ]
+        }
+    }
