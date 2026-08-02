9d40f873c05471ffbd13a192ae84d56755b75142
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-02 10:21:03 -0300

    Criado modelo de response de cliente.

--- src/tcc/api/schemas/clients_schemas.py
+++ src/tcc/api/schemas/clients_schemas.py
@@ -8,7 +8,7 @@ from src.tcc.infrastructure.models.enums.finality_types import ClientFinalityTyp
 from src.tcc.infrastructure.models.enums.property_types import PropertyTypes
 
 
-class CreateClientRequest(BaseModel):
+class ClientResponse(BaseModel):
     id: UUID = Field(
         ...,
         description='ID (UUIDv7) do cliente',
@@ -61,11 +61,88 @@ class CreateClientRequest(BaseModel):
         examples=['Indicação', 'Whatsapp', 'Instagram']
     )
 
+    criado_em: datetime = Field(
+        ...,
+        description='Data e hora da criação do registro do cliente.',
+        examples=['2023-07-21T14:30:00Z']
+    )
+
+    alterado_em: datetime | None = Field(
+        None,
+        description='Data e hora da última alteração do registro do cliente.',
+        examples=['2023-07-22T10:15:00Z']
+    )
+
     model_config = {
         'json_schema_extra': {
             'examples': [
                 {
                     'id': 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+                    'nome': 'João da Silva',
+                    'codigo': '1234',
+                    'numero': '(47) 91234-5678',
+                    'email': 'joao@exemplo.com',
+                    'tipo': 'interessado',
+                    'como_encontrou': 'Indicação',
+                    'criado_em': '2023-07-21T14:30:00Z',
+                    'alterado_em': '2023-07-22T10:15:00Z'
+                }
+            ]
+        }
+    }
+
+
+class CreateClientRequest(BaseModel):
+    nome: str = Field(
+        ...,
+        min_length=3,
+        max_length=60,
+        description='Nome do cliente',
+        examples=['João da Silva']
+    )
+
+    codigo: str = Field(
+        ...,
+        min_length=4,
+        max_length=4,
+        description='Código do cliente',
+        examples=['1234', 'CL01']
+    )
+
+    numero: str = Field(
+        ...,
+        min_length=15,
+        max_length=15,
+        description='Número de telefone do cliente',
+        examples=['(47) 91234-5678', '(47) 98765-4321']
+    )
+
+    email: str = Field(
+        ...,
+        min_length=5,
+        max_length=60,
+        description='Email do cliente',
+        examples=['joao@exemplo.com', 'maria@exemplo.com']
+    )
+
+    tipo: ClientType = Field(
+        ...,
+        max_length=11,
+        description='Tipo do cliente',
+        examples=['interessado', 'cliente']
+    )
+
+    como_encontrou: ContactType | None = Field(
+        None,
+        max_length=18,
+        description='Como o cliente encontrou a empresa',
+        examples=['Indicação', 'Whatsapp', 'Instagram']
+    )
+
+    model_config = {
+        'json_schema_extra': {
+            'examples': [
+                {
                     'nome': 'João da Silva',
                     'codigo': '1234',
                     'numero': '(47) 91234-5678',
