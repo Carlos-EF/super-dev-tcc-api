fcd819c0e5e83a79a72290129225bc8db8dbb26e
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-12 10:21:51 -0300

    Criado função para criar imóvel base.

--- src/tcc/repository/property_repository.py
+++ src/tcc/repository/property_repository.py
@@ -15,3 +15,35 @@ class PropertyRepository:
             session: Session
             ):
         self.session = session
+
+
+    def create(
+            self,
+            property: CreatePropertyRequest
+    ) -> PropertyResponse:
+        property_to_create = PropertyModel(
+            id = uuid7(),
+            propretario_id= property.proprietario,
+            corretor_id= property.corretor,
+            codigo= property.codigo,
+            finalidade= property.finalidade,
+            tipo= property.tipo,
+            em_condominio= property.em_condominio,
+            condominio= property.condominio,
+            cep= property.cep,
+            logradouro= property.logradouro,
+            numero= property.numero,
+            bairro= property.bairro,
+            uf= property.uf,
+            cidade= property.cidade,
+            complemento= property.complemento,
+            valor= property.valor,
+            valor_condominio= property.valor_condominio,
+            valor_iptu= property.valor_iptu,
+            criado_em= datetime.now(),
+        )
+
+        
+        self.session.add(property_to_create)
+        self.session.flush()
+        self.session.commit()
