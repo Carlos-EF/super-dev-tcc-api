4ccbee103a872dcc5da303d62bcc7e27a71b7d2e
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-20 09:28:02 -0300

    Criado rota para editar imagens.

--- src/tcc/api/routes/property_routes.py
+++ src/tcc/api/routes/property_routes.py
@@ -7,7 +7,7 @@ from uuid import UUID
 
 from tcc.infrastructure.connection import get_session, get_storage
 from tcc.repository.property_repository import PropertyRepository
-from tcc.api.schemas.property_schemas import CreatePropertyImageRequest, CreatePropertyRequest, CreateHouseRequest, CreateApartmentRequest, CreateLandRequest, EditPropertyRequest, EditHouseRequest, EditApartmentRequest, EditLandRequest, PropertyResponse, HouseResponse, ApartmentResponse, LandResponse, CompletePropertyResponse, PaginatedPropertyResponse, HouseData, ApartmentData, LandData
+from tcc.api.schemas.property_schemas import CreatePropertyImageRequest, CreatePropertyRequest, CreateHouseRequest, CreateApartmentRequest, CreateLandRequest, EditPropertyImageRequest, EditPropertyRequest, EditHouseRequest, EditApartmentRequest, EditLandRequest, PropertyResponse, HouseResponse, ApartmentResponse, LandResponse, CompletePropertyResponse, PaginatedPropertyResponse, HouseData, ApartmentData, LandData
 
 
 router = APIRouter(
@@ -193,7 +193,7 @@ async def create_image(
         storage=storage
         )
 
-    return repository.create(
+    return repository.create_image(
         image=request,
         file_bytes=file_bytes,
         content_type=file.content_type,
@@ -311,6 +311,34 @@ def edit_apartment(
     )
 
     return edited_apartment
+
+
+@router.put(
+    '/images/{imagem_id}'
+)
+async def edit_image(
+    imagem_id: UUID,
+    request: EditPropertyImageRequest,
+    session: Session = Depends(get_session),
+    storage = Depends(get_storage),
+):
+    repository = PropertyRepository(
+        session=session,
+        storage=storage
+    )
+
+    image = repository.edit_image(
+        imagem_id,
+        request
+    )
+
+    if not image:
+        raise HTTPException(
+            status_code=404,
+            detail='Imagem não encontrada.'
+        )
+
+    return image
 
 
 @router.delete(
